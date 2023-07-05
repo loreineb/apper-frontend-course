988bcefd744a0f5d32f577f3fc9b42e2fe753ad1
loreineb <128940914+loreineb@users.noreply.github.com>
2023-07-05 14:30:28 +0800

    Putting code in reverse.js

--- session-1/reverse.js
+++ session-1/reverse.js
@@ -1,1 +1,22 @@
+// Write a function named reverse that, when given an input string, returns the reversed version of that string. Use traditional for loop. Name this file reverse.js.
 
+function reverse(word) {
+    const container = []
+    for (let i = word.length-1; i > -1; i--){ //#1
+        container.push(word[i])
+    }
+    const oneWord = container.join('') //#2
+    return oneWord
+}
+
+const word = 'hello'
+const reversedWord = reverse(word)
+console.log(reversedWord) // 'olleh'
+
+/*
+Notes to self
+#1 I wasn't getting any output at first
+    -> Reason: I let i = word.length without the -1; don't forget that indexing starts at 0
+#2 the contents of the () in join is the separator, hence ('')
+    -> e.g. if I put join('-'), it would be o-l-l-e-h
+*/
